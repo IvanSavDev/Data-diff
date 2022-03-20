73383c776a488e1cf50072c2ac7217ee6ce0d6f2
IvanSavDev <ivan-savenko@mail.ru>
2022-03-20 23:15:17 +0300

    complite the second point

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
