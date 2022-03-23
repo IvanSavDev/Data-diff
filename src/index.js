ec65dd5ffa1ed240d3fef588178575700f21fb04
IvanSavDev <ivan-savenko@mail.ru>
2022-03-23 21:59:33 +0300

    add functionality for read format .yarm

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import path from 'path';
 import process from 'process';
 import _ from 'lodash';
 import formatingData from './format.js';
+import parse from './parser.js';
 
 const gendiff = (filepath1, filepath2) => {
   const path1 = path.resolve(process.cwd(), filepath1);
   const path2 = path.resolve(process.cwd(), filepath2);
-  const dateForPath1 = JSON.parse(readFileSync(path1), 'UTF-8');
-  const dateForPath2 = JSON.parse(readFileSync(path2), 'UTF-8');
+  const readFile1 = readFileSync(path1, 'UTF-8');
+  const readFile2 = readFileSync(path2, 'UTF-8');
+  const dateForPath1 = parse(path1, readFile1);
+  const dateForPath2 = parse(path2, readFile2);
   const keys = Object.keys(dateForPath1).concat(Object.keys(dateForPath2));
   const deleteDublicate = new Set(keys);
   const arrayKeys = Array.from(deleteDublicate);
---
+++ src/parser.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import yaml from 'js-yaml';
+
+const parse = (fileName, fileData) => {
+  const formatFile = path.extname(fileName).slice(1);
+
+  if (formatFile === 'json') {
+    const dataFromFile = JSON.parse((fileData), 'UTF-8');
+    return dataFromFile;
+  }
+  if (formatFile === 'yaml' || formatFile === 'yml') {
+    const dataFromFile = yaml.load(fileData);
+    return dataFromFile;
+  }
+
+  return `Format not supported ${formatFile}`;
+};
+
+export default parse;
