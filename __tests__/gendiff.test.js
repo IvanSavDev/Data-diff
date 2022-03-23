bf17a7725dd737792b9dfc67e60b8da20f1680aa
IvanSavDev <ivan-savenko@mail.ru>
2022-03-23 21:56:54 +0300

    add test for .yaml format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,14 +3,23 @@ import path from 'path';
 import { readFileSync } from 'fs';
 import gendiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+let __filename;
+let __dirname;
+
+beforeAll(() => {
+  __filename = fileURLToPath(import.meta.url);
+  __dirname = path.dirname(__filename);
+});
 
 const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 const readFile = (fileName) => readFileSync(getFixturePath(fileName), 'UTF-8');
 
-test('gendiff', () => {
-  const result = gendiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'));
-  const correctResult = readFile('expect_file_json.txt').toString();
-  expect(result).toEqual(correctResult);
+test.each([
+  ['filepath1.json', 'filepath2.json', 'expect_file_json.txt'],
+  ['filepath1.yml', 'filepath2.yml', 'expect_file_yaml.txt'],
+  ['filepath1.yaml', 'filepath2.yaml', 'expect_file_yaml.txt'],
+])('gendiff', (file1, file2, expected) => {
+  const result = gendiff(getFixturePath(file1), getFixturePath(file2));
+  const correctResult = readFile(expected).toString();
+  expect(result).toBe(correctResult);
 });
